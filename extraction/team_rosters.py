4e149a843ef6faadd0757d49f3830943b6eeb3bb
cliffe <oluochcliffe13@gmail.com>
2026-03-19 19:19:25 +0300

    Configure extraction script for team rosters

--- extraction/team_rosters.py
+++ extraction/team_rosters.py
@@ -5,9 +5,39 @@ import pandas as pd
 from nba_api.stats.endpoints import commonteamroster
 from nba_api.stats.static import teams
 from extraction.utils import get_snowflake_conn, db_load
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
 logger = logging.getLogger(__name__)
 
+def extract_team_rosters():
+    """Extracts team roster information"""
+    conn = get_snowflake_conn()
+    try:
+        logger.info("Extracting Team Rosters...")
+        nba_teams = teams.get_teams()
+        roster_frames = []
+        current_date = datetime.now().strftime('%Y-%m-%d')
 
+        for index, team in enumerate(nba_teams, 1):
+            time.sleep(0.6) # Add delay for the rate limit
+            if index % 10 == 0:  # Use as progress tracker for every tenth team extracted
+                logger.info(f"Processed rosters for {index}/{len(nba_teams)} teams...")
+            roster = commonteamroster.CommonTeamRoster(
+                team_id=team['id'],
+                season='2024-25'
+                )
+            roster_frames.append(roster.get_data_frames()[0])
+        
+        roster_df = pd.concat(roster_frames, ignore_index=True)
+        roster_df['SNAPSHOT_DATE'] = current_date
+
+        db_load(roster_df, 'raw_team_rosters', conn)
+    
+    except Exception as e:
+        logger.error(f"Error extracting team rosters: {str(e)}")
+        raise
+    finally:
+        conn.close
+        logger.info("Snowflake connection closed.")
